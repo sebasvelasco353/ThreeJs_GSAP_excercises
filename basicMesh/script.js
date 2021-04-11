7a83d8a01473add805abeb687333cfe77f225b5a
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-04-10 20:05:22 -0500

    resizing canvas on window resize

--- basicMesh/script.js
+++ basicMesh/script.js
@@ -1,4 +1,3 @@
-const sceneContainer = document.querySelector('#scene');
 const scene = new THREE.Scene();
 
 scene.background = null;
@@ -15,15 +14,34 @@ camera.position.set(0, 0, 10);
 
 //create a new geometry (object) and material.
 const geometry_one = new THREE.BoxBufferGeometry(2, 2, 2);
-const material_one = new THREE.MeshBasicMaterial();
+const material_one = new THREE.MeshLambertMaterial({color: 0xFFCC00});
 // create a mesh based on the geometry and material
 const cube = new THREE.Mesh(geometry_one, material_one);
+cube.rotation.x = Math.PI/3;
 
 // now we add the element (cube) to the scene
 scene.add(cube);
 
 // now we create the renderer and render the element
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(sceneContainer.clientWidth, sceneContainer.clientHeight);
-sceneContainer.append(renderer.domElement);
-renderer.render(scene, camera);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.append(renderer.domElement);
+
+const light = new THREE.PointLight(0xFFFFFF, 1, 500);
+light.position.set(10, 0, 25);
+scene.add(light);
+
+// renderer function that refreshes every frame
+const render = function() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+render();
+
+// Resize Canvas on window resize!
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+});
