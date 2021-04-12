e1f4af6fa41add4a06bbf0ad267a39abe835d02c
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-04-11 23:35:30 -0500

    container text WIP

--- awwwards replicas/rythmofnature/script.js
+++ awwwards replicas/rythmofnature/script.js
@@ -2,7 +2,12 @@ const tl = gsap.timeline();
 const background_tl = gsap.timeline({paused: true})
 
 tl.from('.header-title', 1.5, {ease: Power1.easeOut, yPercent: 100});
+
+tl.from('.container-title__wrapper', 1, {yPercent: 110});
+tl.from('.container-title', 1, {yPercent: -110}, '-=1');
+
 tl.add(() => {background_tl.play()});
+
 tl.to('header', 1, {ease: Power1.easeOut, x: 0});
 tl.from('.header-link', .5, {ease: Power1.easeOut, yPercent: 100});
 
