c86d53120d982ebfbd8fec82ca0dba9ed5c4d1bc
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-04-11 23:08:34 -0500

    fixed animations

--- awwwards replicas/rythmofnature/script.js
+++ awwwards replicas/rythmofnature/script.js
@@ -1,11 +1,10 @@
 const tl = gsap.timeline();
-const tl2 = gsap.timeline({paused: true})
+const background_tl = gsap.timeline({paused: true})
 
-tl.from('.header-title', 1.5, {ease: Power1.easeOut, y: 100});
-tl.from('.container-title__span', 1.5, {ease: Power1.easeOut, y: 100}, '-=1.5');
-tl.add(() => { tl2.play() });
+tl.from('.header-title', 1.5, {ease: Power1.easeOut, yPercent: 100});
+tl.add(() => {background_tl.play()});
 tl.to('header', 1, {ease: Power1.easeOut, x: 0});
-tl.from('.header-link', 2, {ease: Power1.easeOut, opacity: 0}, '-=.5');
+tl.from('.header-link', .5, {ease: Power1.easeOut, yPercent: 100});
 
-tl2.from('.body-image__wrapper', 1.5, {yPercent: 110});
-tl2.from('.body-image', 1.5, {yPercent: -110}, '-=1.5');
+background_tl.from('.body-image__wrapper', 1.5, {yPercent: 110});
+background_tl.from('.body-image', 1.5, {yPercent: -110}, '-=1.5');
