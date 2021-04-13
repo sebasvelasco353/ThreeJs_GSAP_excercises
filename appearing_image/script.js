73c4be0f6d325f02436101fa9c5db34fdcac1f35
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-04-12 23:15:32 -0500

    deleted all awwwards things because of a nervous breakdown but started other project later because of FOMO

---
+++ appearing_image/script.js
@@ -0,0 +1,28 @@
+const main_tl = gsap.timeline();
+const bg_tl = gsap.timeline();
+
+const textElement = document.querySelector('.title');
+let textElement_split = textElement.innerText.split(" ");
+console.log({textElement_split});
+let newText = "";
+
+
+for(let i=0; i<textElement_split.length; i++){
+    newText += `<div>${textElement_split[i]}</div>`;
+    newText += '&nbsp;';
+}
+
+textElement.innerHTML = newText;
+const targetsDiv = document.querySelectorAll('.title div');
+console.log({targetsDiv});
+TweenMax.staggerFromTo(
+    targetsDiv, 
+    2,
+    {yPercent: 110, ease: 'power2.out'},
+    {yPercent:0, ease: 'power2.out'},
+    .5
+);
+
+bg_tl.from('.image_wrapper', 3.5, {yPercent: 110, ease: "power2.out"});
+bg_tl.from('.image', 3.5, {yPercent: -110, ease: "power2.out"}, "-=3.5");
+bg_tl.to('.title', 1, {xPercent: 110, ease: "power2.inout"});
--- awwwards replicas/README.md
+++
@@ -1,9 +0,0 @@
-# Awwwards replicas
-
-this part of the repo is specifically for replicating awwwards pages, all of them will have
-a link to the respective page in awwwards.
-
-all credits go to the designers and developers of that projects.
-
-## Projects link:
-- [rhythmofnature](https://www.awwwards.com/sites/rhythm-of-nature)
