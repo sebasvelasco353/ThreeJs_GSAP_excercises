c3722e8e21184397a1a9705d57cc700fa6ce2c5c
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-05-08 00:28:49 -0500

    animations

--- hauzzparty/assets/js/index.js
+++ hauzzparty/assets/js/index.js
@@ -1,7 +1,24 @@
-const tl = gsap.timeline();
+const main__timeline = gsap.timeline();
+const logo__timeline = gsap.timeline();
+const welcome__timeline = gsap.timeline();
 
+const home__logo =  document.getElementsByClassName('home__logo');
 const logo__rect = document.getElementsByClassName('logo__rect');
 const logo__text = document.getElementsByClassName('logo__text')
 
-gsap.from(logo__rect, 2, {xPercent: '-500', opacity: 0, ease: "power2.out"});
-gsap.from(logo__text, 2, {xPercent: '100', opacity: 0, ease: "power2.out"});
+const welcome__text = document.getElementsByClassName('welcome__text');
+const welcome__img = document.getElementsByClassName('welcome__img');
+
+const home__buttonDark = document.getElementsByClassName('home__button-dark');
+const home__buttonLight = document.getElementsByClassName('home__button-light');
+
+
+main__timeline.from(logo__rect, 2, {xPercent: '-500', opacity: 0, ease: "power2.out"});
+main__timeline.from(logo__text, 2, {xPercent: '100', opacity: 0, ease: "power2.out"}, '-=2');
+main__timeline.from(home__logo, 1.5, {y: '30vh', ease: "power2.out"});
+
+main__timeline.from(welcome__text, 2, {xPercent: '-100', opacity: 0, ease: "power2.out"});
+main__timeline.from(welcome__img, 2, {xPercent: '100', opacity: 0, ease: "power2.out"}, '-=1.5');
+
+main__timeline.from(home__buttonDark, 2, {xPercent: '-100', opacity: 0, ease: "power2.out"});
+main__timeline.from(home__buttonLight, 2, {xPercent: '100', opacity: 0, ease: "power2.out"}, '-=2');
